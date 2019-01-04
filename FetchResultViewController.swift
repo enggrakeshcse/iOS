0587bdce1acaf6d012056b2949dbe70f486dfdb5
enggrakeshcse <32790967+enggrakeshcse@users.noreply.github.com>
2019-01-04 15:17:27 +0530

    Update FetchResultViewController.swift

--- FetchResultViewController.swift
+++ FetchResultViewController.swift
@@ -1,10 +1,10 @@
-  static  var tempFetchedResultController: NSFetchedResultsController<Contact>?
-    static  var fetchedResultsController: NSFetchedResultsController<Contact> {
+  static  var tempFetchedResultController: NSFetchedResultsController<EntityName>?
+    static  var fetchedResultsController: NSFetchedResultsController<EntityName> {
         if let fetchedResultController = tempFetchedResultController {
             return fetchedResultController
         }
-        let fetchRequest: NSFetchRequest<Contact> = Contact.fetchRequest()
-        let nameSortDescriptor = NSSortDescriptor(key: "firstName", ascending: true)
+        let fetchRequest: NSFetchRequest<EntityName> = EntityName.fetchRequest()
+        let nameSortDescriptor = NSSortDescriptor(key: "attributesName", ascending: true)
         fetchRequest.sortDescriptors = [nameSortDescriptor]
         let fetchResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataBase.shared.context, sectionNameKeyPath: "firstCharact", cacheName: nil)
         tempFetchedResultController = fetchResultsController
